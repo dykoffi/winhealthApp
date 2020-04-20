36b942788b8655db4d167127279ccffb918b2383
dykoffi <koffiedy@gmail.com>
2020-04-20 16:46:00 +0000

    ajout de node_end dans url gestion

--- src/Apps/admin/constants/apiQuery.js
+++ src/Apps/admin/constants/apiQuery.js
@@ -1,5 +1,5 @@
 export const header = {
     timeout: 1000,
-    url : 'https://apiwin.herokuapp.com',
-    //url: process.env.DATABASE_URL ? 'https://apiwin.herokuapp.com': 'http://localhost:8000'
+    //url : 'https://apiwin.herokuapp.com',
+    url: process.env.NODE_ENV === 'production' ? 'https://apiwin.herokuapp.com' : 'http://localhost:8000'
 }
