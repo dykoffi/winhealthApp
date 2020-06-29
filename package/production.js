4fefa7544f167207d9acd5eba22d6f79e16c91da
dykoffi <dykoffi2000@gmail.com>
2020-06-29 16:16:37 +0000

    creation des packages

--- package/production.js
+++ package/production.js
@@ -4,13 +4,13 @@ const https = require('https')
 const favicon = require('express-favicon');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.use(favicon(__dirname + '../build/favicon.ico'));
+app.use(favicon(__dirname + 'build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 https.createServer({
