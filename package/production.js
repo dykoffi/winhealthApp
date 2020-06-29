f225c03c2c91ad50810597cad28ca5a661860c2b
dykoffi <dykoffi2000@gmail.com>
2020-06-29 16:19:27 +0000

    en

--- package/production.js
+++ package/production.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(favicon(__dirname + 'build/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
