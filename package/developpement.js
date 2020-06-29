a3da977111000dd8f2d683327cc05af3796fdbbd
dykoffi <dykoffi2000@gmail.com>
2020-06-29 16:13:01 +0000

    creation des packages

--- package/developpement.js
+++ package/developpement.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '../build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 https.createServer({
