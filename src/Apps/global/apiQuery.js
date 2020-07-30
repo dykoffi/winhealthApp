5391dad8d1e5032247517bcfcf00a4e28f8fb93f
dykoffi <dykoffi2000@gmail.com>
2020-07-30 08:36:59 +0000

    port change

--- src/Apps/global/apiQuery.js
+++ src/Apps/global/apiQuery.js
@@ -1,8 +1,8 @@
 import io from 'socket.io-client';
 const apihostprod = "mail.altea-ci.com:35000"
-const apihosttest = "192.168.16.211:35000"
+const apihosttest = "192.168.16.211:8000"
 const pdfhosttest = "mail.altea-ci.com:36000"
-const pdfhostprod = "192.168.16.211:36000"
+const pdfhostprod = "192.168.16.211:3001"
 
 export const header = {
     timeout: 1000,
