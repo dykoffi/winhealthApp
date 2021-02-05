324b85f3f292609c650aba192efb0f9564a7aeac
dykoffi <dykoffi2000@gmail.com>
2021-02-05 17:13:47 +0000

    ok serve

--- src/Apps/global/apiQuery.js
+++ src/Apps/global/apiQuery.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 const apihostprod = `mail.altea-ci.com:35000`
-const apihosttest = `${process.env.API_HOST}:8000`
+const apihosttest = `${window.location.hostname}:8000`
 export const header = {
     timeout: 1000,
     url: process.env.NODE_ENV === 'production' ? `http://${apihostprod}` : `http://${apihosttest}`,
